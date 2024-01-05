bdb21b0bcf6ec66ee9c33c743b306118bc085e04
GOPINATH DEY <98454940+gopidey512@users.noreply.github.com>
2024-01-05 11:02:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-  strings: ["Frontend Developer", "Backend Developer", "UI/UX Designer"],
+  strings: ["Frontend Developer", "Backend Developer", "UI Designer"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
